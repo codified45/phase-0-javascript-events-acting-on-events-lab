ca1338dd7fca4a7725ef173c5c5b3fc99911580a
Jack Lach <jack45k@gmail.com>
2022-12-31 01:33:38 -0500

    commit tree 8a564cbbab928bec7e5b5762057e3badbd232511

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+
+};
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+};
+
+document.addEventListener('keydown', function(event) {
+        if (event.key === 'ArrowLeft') {
+            moveDodgerLeft();
+        }
+        if (event.key === 'ArrowRight') {
+            moveDodgerRight();
+        }
+    }
+);
+
+
+
+
+
+// const dodger = document.getElementById('dodger');
+// undefined
+// dodger.style.backgroundColor = '#000000';
+// '#000000'
+// dodger.style.backgroundColor = '#FF69B4';
+// '#FF69B4'
+// dodger.style.bottom = '100px';
+// '100px'
+// dodger.style.bottom = '0px'
+// '0px'
+// dodger.style.left = '0px';
