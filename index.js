2bfe8f25f104c62e4ddf45dfc464dfbbd47b29cd
Jack Lach <jack.lach4@gmail.com>
2023-02-06 04:09:54 -0500

    phase-1 version, first test passing

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+document.addEventListener("keydown", function (e) {
+        if (e.key === "ArrowLeft") {
+            moveDodgerLeft();
+        };
+        if (e.key === "ArrowRight") {
+            moveDodgerLeft();
+        };
+    });
+
+
+function moveDodgerLeft () {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+};
