7c9ed9f9ee7ffa082377999afc621692d287c03a
Jack Lach <jack.lach4@gmail.com>
2023-02-06 04:19:50 -0500

    phase-1, both tests passing

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ document.addEventListener("keydown", function (e) {
             moveDodgerLeft();
         };
         if (e.key === "ArrowRight") {
-            moveDodgerLeft();
+            moveDodgerRight();
         };
     });
 
@@ -17,5 +17,14 @@ function moveDodgerLeft () {
 
     if (left > 0) {
         dodger.style.left = `${left - 1}px`;
+    }
+};
+
+function moveDodgerRight () {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
     }
 };
